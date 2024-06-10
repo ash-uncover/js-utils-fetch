bf0b8038991d9b1605dd0e6842b05ab994cb9268
Antoine MAILLARD <ash.uncover@gmail.com>
2024-06-10 17:17:51 +0200

    Simplify fetch wrapping

--- src/service.ts
+++ src/service.ts
@@ -31,32 +31,13 @@ export class Service {
   }
 
   async fetch(pathUrl: string, options: any) {
-    const hasBody = options.body && (options.method === 'POST' || options.method === 'PUT')
-
-    // Build headers
-    const headers = new Headers()
-    if (hasBody) {
-      headers.set('content-type', options.contentType || 'application/json; charset=UTF-8')
-    }
-    headers.set('accept', options.accept || 'application/json')
-    headers.set('accept-encoding', options.acceptEncoding || 'gzip, deflate, br')
-    headers.set('connection', options.connection || 'keep-alive')
-
     const url = this.buildUrl(pathUrl)
 
-    const request: RequestInit = {
-      method: options.method,
-      headers,
-    }
-    if (hasBody) {
-      request.body = options.body
-    }
+    this._config.runBeforeHooks(url, options)
 
-    this._config.runBeforeHooks(url, request)
+    const response = await fetch(url, options)
 
-    const response = await fetch(url, request)
-
-    this._config.runAfterHooks(url, request, response)
+    this._config.runAfterHooks(url, options, response)
 
     return response
   }
